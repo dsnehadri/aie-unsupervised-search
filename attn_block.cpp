1e2159d1820597f34adfaac01a5a222180fdd06e
Snehadri Das <snehadri@uchicago.edu>
2026-03-12 21:31:47 -0500

    add reshaping

--- attn_block.cpp
+++ attn_block.cpp
@@ -1,10 +1,6 @@
-#include "attn_block_types.h"
+#include "attn_helpers.h"
 
 // object self-attention
-
-// ffn_w, ffn_b are FFN linear layers
-// ffn_ln_g, ffn_ln_b are layernorms per layer
-// post_ffn_ln_g, post_ffn_ln_b are final layer norms after FFN skip
 
 void attn_block_obj(
     data_t x[N_MAX][E_DIM], // input embeddings, which are modified in place
@@ -20,9 +16,8 @@ void attn_block_obj(
     const weight_t Wq[E_DIM][E_DIM], const weight_t bq[E_DIM],
     const weight_t Wk[E_DIM][E_DIM], const weight_t bk[E_DIM],
     const weight_t Wv[E_DIM][E_DIM], const weight_t bv[E_DIM],
-    const weight_t bias_k_param[E_DIM], // add_bias_kv learned token
-    const weight_t bias_v_param[E_DIM],
-
+    const weight_t bias_k[E_DIM], // add_bias_kv learned token
+    const weight_t bias_v[E_DIM],
     const weight_t Wo[E_DIM][E_DIM], const weight_t bo[E_DIM], // output projections
 
     // post attention layer norm
@@ -40,6 +35,8 @@ void attn_block_obj(
     const ln_param_t post_ffn_b[E_DIM]
 
 ) {
+
+
     // TODO: add interface pragmas i do system integration
     #pragma HLS INTERFACE ap_memory port = x
     #pragma HLS_INTERFACE ap_memory post = Wq
@@ -55,3 +52,42 @@ void attn_block_obj(
 
 }
 
+// reshape QKV into per head arrays and append bias_kv
+template <int N_Q, int N_KEY>
+void reshape_and_append_bias_kv(
+    const data_t Q_full[N_Q][E_DIM],
+    const data_t K_full[N_KEY][E_DIM],
+    const data_t V_full[N_KEY][E_DIM],
+    const weight_t bias_k[E_DIM],
+    const weight_t bias_v[E_DIM],
+    data_t Q_h[N_HEADS][N_Q][D_HEAD],
+    data_t K_h[N_HEADS][N_KEY+1][D_HEAD],
+    data_t V_h[N_HEADS][N_KEY+1][D_HEAD]
+) {
+    RESHAPE:
+    for (int h = 0; h <N_HEADS; h++) {
+        #pragma HLS UNROLL
+        for (int i = 0; i < N_Q; i++) {
+            for (int d = 0; d < D_HEAD; d++) {
+                #pragma HLS PIPELINE II=1
+                Q_h[h][i][d] = Q_full[i][h*D_HEAD+d];
+            }
+        }
+        // separate from above because for cross attention N_KEY != N_Q
+        for (int i = 0; i < N_Q, i++) {
+            for (int d = 0; d < D_HEAD; d++) {
+                #pragma HLS PIPELINE II = 1
+                int e = h * D_HEAD + d;
+                K_h[h][i][d] = K_full[i][e];
+                V_h[h][i][d] = V_full[i][e];
+            }
+        }
+        
+        for (int d = 0; d < D_HEAD; d++) {
+            #pragma HLS_PIPELINE II=1
+            int e = h * D_HEAD + d;
+            K_h[h][N_KEY][d] = (data_t)bias_k[e];
+            V_h[h][N_KEY][d] = (data_t)bias_v[e];
+        }
+    }
+}
