1450b60dec3f62ec7280f4bdcd36331a733974be
Snehadri Das <snehadri@uchicago.edu>
2026-03-28 18:53:17 -0500

    move tcl files into base directory

--- run_csim_embed_ffn.tcl
+++ run_csim_embed_ffn.tcl
@@ -2,6 +2,7 @@ open_project embed_ffn_proj
 set_top embed_ffn_top
 add_files embed_ffn_source/embed_ffn.cpp 
 add_files embed_ffn_source/embed_ffn.h 
+add_files dnn_block_source/dnn_block.h
 add_files attn_block_source/attn_helpers.h
 add_files attn_block_source/attn_block_types.h
 add_files -tb embed_ffn_source/embed_ffn_tb.cpp
