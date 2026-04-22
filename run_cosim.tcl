77b3b04d62ecc93e3b56fce77731676ed3bf02df
Snehadri Das <snehadri@uchicago.edu>
2026-04-22 12:25:49 -0500

    update tcls

--- run_cosim.tcl
+++ run_cosim.tcl
@@ -1,22 +1,16 @@
-# usage: /home/snehadri/Vitis_HLS/2022.2/bin/vitis_hls -f run_csim_passwd_stream.tcl
-
 open_project passwd_stream_proj
 set_top passwd_stream_top
 open_solution "solution1"
 set_part {xcvc1902-vsva2197-2MP-e-S}
 create_clock -period 5 -name default
 
-# source files
 add_files passwd_stream_source/passwd_stream_top.cpp
 
-# testbench files
-
 add_files -tb test_benches/passwd_stream_tb.cpp
-add_files -tb test_benches/tb_helpers.h
 add_files -tb cnpy/cnpy.cpp
 add_files -tb cnpy/cnpy.h
 
-# run csim
-
 csim_design -ldflags "-lz" -clean
+csynth_design
+cosim_design -ldflags "-lz"
 exit
---
+++ run_export.tcl
@@ -0,0 +1,13 @@
+open_project passwd_stream
+open_solution solution1
+set_part {xcvc1902-vsva2197-2MP-e-S}
+set_top passwd_stream_top
+add_files passwd_stream_source/passwd_stream_top.cpp
+add_files -tb test_benches/passwd_stream_tb.cpp
+add_files -tb test_benches/tb_helpers.h
+add_files -tb cnpy/cnpy.cpp
+add_files -tb cnpy/cnpy.h
+
+config_export -format xo
+csynth_design
+export_design -format xo -output passwd_stream_top.xo
